e799a26e68ad62a5df13927efd76596bfad58c7b
Juan de Urtubey <jdeurt@gmail.com>
2022-12-27 19:46:16 -0600

    fix: attempt to fix dos

--- src/util/use-promise.ts
+++ src/util/use-promise.ts
@@ -2,9 +2,15 @@ import { useEffect, useState } from "react";
 
 export function usePromise<T>(promise: Promise<T>): T | undefined {
     const [value, setValue] = useState<T>();
+    const [isFullfilled, setIsFullfilled] = useState(false);
 
     useEffect(() => {
-        promise.then(setValue);
+        if (isFullfilled) return;
+
+        promise.then((val) => {
+            setIsFullfilled(true);
+            setValue(val);
+        });
     }, [promise]);
 
     return value;
